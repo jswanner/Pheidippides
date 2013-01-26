3b87d304038745f1fc00f38be6ed3c9118403c54
Jacob Swanner <jacob@envylabs.com>
2013-01-25 19:00:28 -0500

    Explicitly set html and text

--- app/mailers/relay_mailer.rb
+++ app/mailers/relay_mailer.rb
@@ -1,6 +1,13 @@
 class RelayMailer < ActionMailer::Base
   def relay mail_params, mail_headers
+    html = mail_params.delete :html
+    text = mail_params.delete :text
+
     headers mail_headers
-    mail mail_params
+
+    mail mail_params do |format|
+      format.html { render html: html }
+      format.text { render text: text }
+    end
   end
 end
